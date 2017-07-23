2331a7de5f48ed3416e275dc8338b2cafc2ece87
calvin echols <calvin.echols@gmail.com>
2017-07-23 17:04:26 -0400

    Fixed e2e test.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('learning-angular App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
   });
 });
